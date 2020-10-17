64c03865693e022447eb669fa30d699b19bcd761
Alex <glopezalex631@gmail.com>
2020-10-17 00:31:40 -0500

    Cards y login

--- src/app/components/propuesta-card/propuesta-card.component.ts
+++ src/app/components/propuesta-card/propuesta-card.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-propuesta-card',
   templateUrl: './propuesta-card.component.html',
   styleUrls: ['./propuesta-card.component.css']
 })
 export class PropuestaCardComponent implements OnInit {
-
+  faChevronLeft = faChevronLeft;
   constructor() { }
 
   ngOnInit(): void {
